2b6c49c4ea934c14f8f6c2f9646f9b8a660008f2
trineesha thathsarani <trineeshasanduni125@gmail.com>
2023-09-19 13:32:08 +0530

    search

--- app/src/main/java/com/example/database/TodoList.kt
+++ app/src/main/java/com/example/database/TodoList.kt
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter
 
 import android.widget.ImageView
 import android.widget.TextView
+import java.util.Locale
 
 
 class TodoList(context: Context, resource: Int, todos: List<ToDo>) :
@@ -16,6 +17,15 @@ class TodoList(context: Context, resource: Int, todos: List<ToDo>) :
     private val context: Context = context
     private val resource: Int = resource
     private val todos: List<ToDo> = todos
+    private var filteredTodos: List<ToDo> = todos
+
+    override fun getCount(): Int {
+        return filteredTodos.size
+    }
+
+    override fun getItem(position: Int): ToDo? {
+        return filteredTodos[position]
+    }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val inflater = LayoutInflater.from(context)
@@ -25,7 +35,19 @@ class TodoList(context: Context, resource: Int, todos: List<ToDo>) :
         val description = row.findViewById<TextView>(R.id.description)
         val imageView = row.findViewById<ImageView>(R.id.onGoing)
 
-        val toDo = todos[position]
+//        val toDo = todos[position]
+//        title.text = toDo.title
+//        description.text = toDo.description
+//        imageView.visibility = View.INVISIBLE
+//
+//        if (toDo.finished > 0) {
+//            imageView.visibility = View.VISIBLE
+//        }
+//
+//
+//        return row
+//    }
+        val toDo = filteredTodos[position]
         title.text = toDo.title
         description.text = toDo.description
         imageView.visibility = View.INVISIBLE
@@ -34,7 +56,27 @@ class TodoList(context: Context, resource: Int, todos: List<ToDo>) :
             imageView.visibility = View.VISIBLE
         }
 
-
         return row
     }
+
+    // Filter the list based on the search query
+    fun filter(query: CharSequence?) {
+        val filteredList = mutableListOf<ToDo>()
+
+        if (!query.isNullOrEmpty()) {
+            val searchQuery = query.toString().toLowerCase(Locale.getDefault())
+            for (todo in todos) {
+                if (todo.title?.toLowerCase(Locale.getDefault())?.contains(searchQuery) == true ||
+                    todo.description?.toLowerCase(Locale.getDefault())?.contains(searchQuery) == true) {
+                    filteredList.add(todo)
+                }
+            }
+        } else {
+            filteredList.addAll(todos)
+        }
+
+        filteredTodos = filteredList
+        notifyDataSetChanged()
+    }
+
 }
